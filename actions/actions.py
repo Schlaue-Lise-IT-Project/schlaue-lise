d28a6adc9ad84fff68d24fc71e1d06dc313870c2
Lena-Holderrieth <94544385+Lena-Holderrieth@users.noreply.github.com>
2021-11-19 17:53:14 +0100

    URL Push

--- actions/actions.py
+++ actions/actions.py
@@ -1,4 +1,5 @@
 
+import collections
 import logging
 from typing import Any, Optional, Text, Dict, List
 from rasa_sdk.executor import CollectingDispatcher
@@ -48,7 +49,7 @@ class ValidateInformation(FormValidationAction):
             dispatcher.utter_message(text="Es tut mir Leid, die Eingabe für dein >>Geschlecht<< wurde nicht erkannt. Bitte gib an, ob du dich als 'männlich', 'weiblich' oder 'divers' bezeichnen würdest.")
             return {"geschlecht": None}
 
-    def validate_haustierhalter(
+    def validate_haustier(
         self,
         slot_value: Any,
         dispatcher: CollectingDispatcher,
@@ -129,5 +130,48 @@ class ActionAnswerHygiene(Action):
         answer += f"\nFindest du bei diesen Stellen:"
 
         dispatcher.utter_message(text=answer)
+
+        return []
+
+class ActionAnswerURL(Action):
+    def name(self):
+        return "action_answer_url"
+
+    def run(
+        self, 
+        dispatcher: "CollectingDispatcher", 
+        tracker: Tracker, 
+        domain: "DomainDict"
+    ) -> List[Dict[Text, Any]]:
+        url = "Bitte hier klicken, um direkt zur Antwort zu springen:\n"
+
+        slots = tracker.current_slot_values()
+
+        url += f"\nwww.schlaue-lise.de/"
+       
+        count = 0
+        valueCount = 0
+
+        for slot in slots:
+            slotValue = tracker.get_slot(slot)
+            if slotValue != None:
+                if count > 0:
+                    url+= f"&{slot}="
+                else: 
+                    url += f"?{slot}="
+                if isinstance(slotValue, collections.Iterable) & isinstance(slotValue, list):
+                    for value in slotValue:
+                        if valueCount > 0:
+                            url += f"+{value}"
+                            valueCount += 1
+                        else:
+                            url += f"{value}"
+                            valueCount += 1
+                    valueCount = 0
+                else:
+                    url += f"{slotValue}"
+                count += 1
+
+        dispatcher.utter_message(text=url)
 
         return []
